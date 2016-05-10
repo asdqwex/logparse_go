87029f5f98e27640d5c8427594a47f37bbedf342
asdqwex <matthew.ellsworth@matthewellsworth.com>
2016-05-09 21:28:45 -0600

    we now pass an array of pointers to the argument structures

--- logparse.go
+++ logparse.go
@@ -6,7 +6,14 @@ import (
     "github.com/asdqwex/argparse_go"
 )
 
+a := Argument{"takes_input": 'string', "longname": 'function1', "required": 0, "conflicts": [], "dependencies": [], "only_once": 0, "reads_from_stdin": 0, "hungry": 1}
+b := Argument{"takes_input": nil', "longname": 'function2', "required": 0, "conflicts": ['a'], "dependencies": ['c'], "only_once": 1, "reads_from_stdin": 0, "hungry": 0}
+c := Argument{"takes_input": 'string', "longname": 'function3', "required": 0, "conflicts": ['a'], "dependencies": ['b'], "only_once": 1, "reads_from_stdin": 0, "hungry": 1}
+d := Argument{"takes_input": nil, "longname": 'function4', "required": 1, "conflicts": [], "dependencies": [], "only_once": 1, "reads_from_stdin": 1, "hungry": 1}
+
+arguments := [*a, *b, *c, *d]
+
 func main() {
-  fmt.Print(argparse.Parse(os.Args))
+  fmt.Print(argparse.Parse(os.Args, arguments))
 
 }
