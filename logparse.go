2632e275d070b27651c90ce05ce821a7c2bed068
asdqwex <matthew.ellsworth@matthewellsworth.com>
2016-05-10 02:26:05 -0600

    more useage thoughts

--- logparse.go
+++ logparse.go
@@ -1,23 +1,26 @@
-// logparse - generate dynamic hash maps from log files
+// logparse.go - generate dynamic hash maps from log files
 //
+// a practice project by Matthew Ellsworth - https://github.com/asdqwex
 //
+// logparse [options] [outfile] <infile>
 //
-// logparse -pcv -t nginx -k 0.0.0.0 -k GET -f total -d 01012016 -d 02022016 -- data1.log data2.log data3.log
-// logparse --print --color --verbose --type nginx --key 0.0.0.0 --key GET --type total --date 01012016 --date 02022016 -- data1.log data2.log data3.log
+// logparse -cv -t nginx -k 0.0.0.0 -k GET -f total -d 01012016 -d 02022016  -o outfile.log -- data1.log data2.log data3.log
+// logparse --verbose --type=nginx --key=0.0.0.0 --key=GET --type=total --date=01012016 --date=02022016 --outfile=outfile.log -- data1.log data2.log data3.log
 //
-// -p = print output to stdout
+// -o = outfile
 // -c = colored output
 // -v = versbose: display logic assertions
 // -t = type: nginx || apache || varnish || raw
 // -k = key: regex match to use as map key, in order of desired nesting
-// -f = function to use when generatin gvalues: total || found || percent
+// -f = function to use when generating values: total || found || percent
 // -d = date boundries
 // -- = files to read from
 //
-// logparse --print --color --verbose --type nginx --key 0.0.0.0 --key GET --type total --date 01012016 --date 02022016 - < cat data1.log
+// logparse --color --verbose --type=nginx --key=0.0.0.0 --key=GET --type=total --date=01012016 --date=02022016 - < cat data1.log
 //
 // same as above but - means read from stdin
 //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 package main
 
@@ -27,14 +30,57 @@ import (
     "github.com/asdqwex/argparse_go"
 )
 
-a := Argument{"takes_input": 'string', "longname": 'function1', "required": 0, "conflicts": [], "dependencies": [], "only_once": 0, "reads_from_stdin": 0, "hungry": 1}
-b := Argument{"takes_input": nil', "longname": 'function2', "required": 0, "conflicts": ['a'], "dependencies": ['c'], "only_once": 1, "reads_from_stdin": 0, "hungry": 0}
-c := Argument{"takes_input": 'string', "longname": 'function3', "required": 0, "conflicts": ['a'], "dependencies": ['b'], "only_once": 1, "reads_from_stdin": 0, "hungry": 1}
-d := Argument{"takes_input": nil, "longname": 'function4', "required": 1, "conflicts": [], "dependencies": [], "only_once": 1, "reads_from_stdin": 1, "hungry": 1}
+a := argparse.Argument{"takes_input": 'string', "longname": 'function1', "required": 0, "conflicts": ['a', 'b'], "dependencies": ['c'], "only_once": 0, "reads_from_stdin": 0, "hungry": 1}
+b := argparse.Argument{"takes_input": nil', "longname": 'function2', "required": 0, "conflicts": ['a'], "dependencies": ['c'], "only_once": 1, "reads_from_stdin": 0, "hungry": 0}
+c := argparse.Argument{"takes_input": 'string', "longname": 'function3', "required": 0, "conflicts": ['a'], "dependencies": ['b'], "only_once": 1, "reads_from_stdin": 0, "hungry": 1}
+d := argparse.Argument{"takes_input": nil, "longname": 'function4', "required": 1, "conflicts": [], "dependencies": [], "only_once": 1, "reads_from_stdin": 1, "hungry": 1}
 
 arguments := [*a, *b, *c, *d]
 
 func main() {
-  fmt.Print(argparse.Parse(os.Args, arguments))
+  options := argparse.Parse(os.Args, arguments)
+  // for option in options
+    // if the option exists
+      // run function with provided data
+    // else "ERROR :  no function by that name"
+}
+
+// -p = print output to stdout
+func print (bool) {
+
+}
+
+// -c = colored output
+func color (bool, string) {
+
+}
+
+// -v = versbose: display logic assertions
+func verbose (bool) {
+
+}
+
+// -t = type: nginx || apache || varnish || raw
+func file_type (string) {
+
+}
+
+// -k = key: regex match to use as map key, in order of desired nesting
+func key (string) {
+
+}
+
+// -f = function to use when generatin gvalues: total || found || percent
+func function_type (string) {
+
+}
+
+// -d = date boundries
+func date (string) {
+
+}
+
+// -- = files to read from
+func double_dash (bool, string) {
 
 }
