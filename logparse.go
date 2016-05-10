35008ddc2384bc2829ba0bd9a7ae458de16b8c00
asdqwex <matthew.ellsworth@matthewellsworth.com>
2016-05-10 01:25:56 -0600

    starting POSIX compliant rewrite and added license

--- logparse.go
+++ logparse.go
@@ -1,3 +1,24 @@
+// logparse - generate dynamic hash maps from log files
+//
+//
+//
+// logparse -pcv -t nginx -k 0.0.0.0 -k GET -f total -d 01012016 -d 02022016 -- data1.log data2.log data3.log
+// logparse --print --color --verbose --type nginx --key 0.0.0.0 --key GET --type total --date 01012016 --date 02022016 -- data1.log data2.log data3.log
+//
+// -p = print output to stdout
+// -c = colored output
+// -v = versbose: display logic assertions
+// -t = type: nginx || apache || varnish || raw
+// -k = key: regex match to use as map key, in order of desired nesting
+// -f = function to use when generatin gvalues: total || found || percent
+// -d = date boundries
+// -- = files to read from
+//
+// logparse --print --color --verbose --type nginx --key 0.0.0.0 --key GET --type total --date 01012016 --date 02022016 - < cat data1.log
+//
+// same as above but - means read from stdin
+//
+
 package main
 
 import (
