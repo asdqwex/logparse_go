934a72b1951d20c35368a3fdb09cc562f97d5354
asdqwex <matthew.ellsworth@matthewellsworth.com>
2016-05-10 21:35:05 -0600

    logparse is the only thing that needs to know what its arguments do

--- logparse.go
+++ logparse.go
@@ -4,7 +4,7 @@
 //
 // logparse [options] [outfile] <infile>
 //
-// logparse -cv -t nginx -k 0.0.0.0 -k GET -f total -d 01012016 -d 02022016  -o outfile.log -- data1.log data2.log data3.log
+// logparse -cv -t nginx -k 0.0.0.0 -k GET -f total -d 01012016 -d 02022016  -o outfile.log data1.log data2.log data3.log
 // logparse --verbose --type=nginx --key=0.0.0.0 --key=GET --type=total --date=01012016 --date=02022016 --outfile=outfile.log -- data1.log data2.log data3.log
 //
 // -o = outfile
@@ -16,7 +16,6 @@
 // -d = date boundries
 // -- = files to read from
 //
-// logparse --color --verbose --type=nginx --key=0.0.0.0 --key=GET --type=total --date=01012016 --date=02022016 - < cat data1.log
 //
 // same as above but - means read from stdin
 //
@@ -30,23 +29,56 @@ import (
     "github.com/asdqwex/argparse_go"
 )
 
+type Argument struct {
+    takes_input string
+    longname  string
+    required bool
+    conflicts []string
+    dependencies []string
+    only_once bool
+    reads_from_stdin bool
+    hungry bool
+}
+
+// Example arg_def_map
+//  my_apps_args = {
+//    a = {
+//      takes_input = 'string' # type of data this function needs
+//      longname = 'function1' # if called with -- what is it called
+//      required = 0           # does this option always need to be defined
+//      conflicts = ['b', 'd'] # other options this function cannot be run with
+//      dependencies = ['a']   # other options this function requires
+//      only_once = 1          # this function should only be called once
+//      reads_from_stdin: 0    # this function read from stdin
+//      hungry = 1             # this function takes the rest of the input data
+//    },
+//    b = {
+//      ...
+//      ...
+//    },
+//    c = {
+//      ...
+//      ...
+//    }
+// }
+
 a := argparse.Argument{"takes_input": 'string', "longname": 'function1', "required": 0, "conflicts": ['a', 'b'], "dependencies": ['c'], "only_once": 0, "reads_from_stdin": 0, "hungry": 1}
 b := argparse.Argument{"takes_input": nil', "longname": 'function2', "required": 0, "conflicts": ['a'], "dependencies": ['c'], "only_once": 1, "reads_from_stdin": 0, "hungry": 0}
 c := argparse.Argument{"takes_input": 'string', "longname": 'function3', "required": 0, "conflicts": ['a'], "dependencies": ['b'], "only_once": 1, "reads_from_stdin": 0, "hungry": 1}
 d := argparse.Argument{"takes_input": nil, "longname": 'function4', "required": 1, "conflicts": [], "dependencies": [], "only_once": 1, "reads_from_stdin": 1, "hungry": 1}
 
-arguments := [*a, *b, *c, *d]
+modules := [*a, *b, *c, *d]
 
 func main() {
-  options := argparse.Parse(os.Args, arguments)
+  options := argparse.Parse(os.Args)
   // for option in options
     // if the option exists
       // run function with provided data
     // else "ERROR :  no function by that name"
 }
 
-// -p = print output to stdout
-func print (bool) {
+// -o = print output to file
+func outfile (string) {
 
 }
 
